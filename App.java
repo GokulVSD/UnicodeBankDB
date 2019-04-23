c230a961c83a75f578ef459595ae02863b59df47
GokulVSD <gokul.vasda@gmail.com>
2019-04-23 17:34:26 +0530

    Added a lot of resources

--- App.java
+++ App.java
@@ -7,10 +7,18 @@ public class App {
         staticFileLocation("/public");
         CustomerDB c = new DBD();
         AccountDB d = new DBD();
+        c.createNewCustomer("administrator");
         get("/", (req, res) -> {
             res.redirect("/login.html");
             return " ";
         });
+        post("/login", (req, res) -> {
+            String username = req.queryParams("username");
+            String password = req.queryParams("password");
 
+            if(username.equals("administrator") && password.equals("123")){
+                return " ";
+            }
+        });
     }
 }
